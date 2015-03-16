b3feba5ba30cac8c4f0ea880d7dbe595fa569083
TobiasSchultz <tobias.g.schultz@gmail.com>
2015-03-16 13:18:30 +0100

    adwfedgrhtfjgykuhijo

---
+++ Webside Iterasjon 2/header.php
@@ -0,0 +1,18 @@
+<?php 
+
+echo "<!DOCTYE html>
+<html>
+	<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/generellStil.css">
+	<link rel="stylesheet" type="text/css" href="css/bokser.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+	<link rel="stylesheet" type="text/css" href="css/kalender.css">
+	<link rel="stylesheet" type="text/css" href="css/login.css">
+	<link rel="stylesheet" type="text/css" href="css/navBar.css">
+	<link rel="stylesheet" type="text/css" href="css/personerOgProsjektor.css">
+	<link rel="stylesheet" type="text/css" href="css/reservasjonsbekreftelse.css">
+	</head>
+<body>";
+
+?>
