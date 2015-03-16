82c31f5f3d79a78bf31bdc91360637539a32cd6f
TobiasSchultz <tobias.g.schultz@gmail.com>
2015-03-16 13:33:25 +0100

    Endret header

--- Webside Iterasjon 2/header.php
+++ Webside Iterasjon 2/header.php
@@ -1,9 +1,11 @@
 <?php 
 
-echo "<!DOCTYE html>
+echo '<!DOCTYE html>
 <html>
 	<head>
 	<meta charset="utf-8">
+
+	//	STILARK
 	<link rel="stylesheet" type="text/css" href="css/generellStil.css">
 	<link rel="stylesheet" type="text/css" href="css/bokser.css">
 	<link rel="stylesheet" type="text/css" href="css/index.css">
@@ -12,7 +14,12 @@ echo "<!DOCTYE html>
 	<link rel="stylesheet" type="text/css" href="css/navBar.css">
 	<link rel="stylesheet" type="text/css" href="css/personerOgProsjektor.css">
 	<link rel="stylesheet" type="text/css" href="css/reservasjonsbekreftelse.css">
+
+	//	FONTER
+	<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
+
 	</head>
-<body>";
+<body>'
+;
 
 ?>
