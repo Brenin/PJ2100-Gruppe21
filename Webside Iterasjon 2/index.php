4fd14feb095f920aea32cceeaa486745013a4da5
Ibenoliver <ibenoliver@hotmail.com>
2015-03-18 11:20:38 +0100

    commit

--- Webside Iterasjon 2/index.php
+++ Webside Iterasjon 2/index.php
@@ -1,48 +1,133 @@
 <?php
 
-	include_once("user.php");
+  include_once("user.php");
 
-	if (isset($_POST["submit"])) {
-		$Email = $_POST["user"];
-		$Password = $_POST["pass"];
+  if (isset($_POST["submit"])) {
+    $Email = $_POST["user"];
+    $Password = $_POST["pass"];
 
-		$object = new User();
-		$object->oliibe14($Email, $Password);
-	}
+    $object = new User();
+    $object->oliibe14($Email, $Password);
+  }
 ?>
 
+<!DOCTYPE html>
 <html>
-	<head>
-	<meta charset="utf-8">
-	<title>Innlogging</title>
-	<meta name="description" content="booking">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
-	<link rel="stylesheet" href="css/datepicker.css">
-	<link rel="stylesheet" href="css/login.css">
-	<link rel="stylesheet" href="css/style.css"> 
+    
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/containere.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/kalender.css">
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link rel="stylesheet" type="text/css" href="css/navBar.css">
+    <link rel="stylesheet" type="text/css" href="css/personerOgProsjektor.css">
+    <link rel="stylesheet" type="text/css" href="css/reservasjonsbekreftelse.css">
+    <script type="text/javascript" src="js/jQuery-1.4.2.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+</head>
+<body>
+    
+<section id="header">
+    <div id="logo">
+    <img id="westerdals" src="bilder/logo2.jpg" />
+    </div>
 
-	</head>
-	<body>
-		<div class="dude">
-			<div class="container">
-			<div class="well">
-				<form method="post" action="index.php">
-					<h4>Username: <input type="text" class="form-control" id="inputEmail" placeholder="Epost" name="user"></h4>
-					<h4>Password: <input type="password" class="form-control" id="inputPassword" placeholder="Passord" name="pass"></h4>
-					<input type="submit" class="btn btn-primary" value="submit" name="submit">Log In</input>
-			</div>
-		</div>
-	</div>
+</section>    
 
+<section id="infoOgBooking">
+    <div class="bokser" id="infoOmBooking">
+        <h4>Book grupperom på CK32</h4>
+        <p>Velkommen til Booking av grupperom ved CK32. Klikk på "Book grupperom nå" for å reservere et grupperom.</p>
+    </div>
+    <a href="login.html"><div class="bokser" id="bookingBoks"><br>BOOK GRUPPEROM NÅ</div></a>
+    
+</section>
+    
+    <!-- FAQ-delen av siden. -->
 
+<section id="faqSeksjon">
+    <div id="beskrivelse">
+    <h1>FAQ - Frequently Asked Questions</h1>
+  <p>Trykk på spørsmålene for å se svar.</p>
+  </div>
+      <div id="faq">
+  <section class="spørsmål">
+    <span>Q: </span>Hvor lenge kan en booke et rom?
+  </section>
+  <section class="svar">
+    <span>A: </span>En elev kan booke et rom intill 3 timer om gangen. 
+  </section>
+  
+  <section class="spørsmål">
+   <span>Q: </span>Hvor mange rom kan en booke?
+  </section>
+  <section class="svar">
+   <span>A: </span>En kan booke ett rom om gangen!
+  </section>
+      
+    <section class="spørsmål">
+   <span>Q: </span>Hvor mange kan bruke et rom?
+  </section>
+  <section class="svar">
+   <span>A: </span>Vi har rom for to, tre og fire personer.
+  </section>
+      
+  <section class="spørsmål">
+   <span>Q: </span>Hvordan vet jeg at bookingen var vellykket?
+  </section>
+  <section class="svar">
+   <span>A: </span>Dersom bookingen var vellykket har du fått en mail på ItsLearning.
+  </section>
+      
+      <section class="spørsmål">
+   <span>Q: </span>Hvordan kan jeg booke et rom?
+  </section>
+  <section class="svar">
+   <span>A: </span>Du kan booke et rom ved å gå inn på <a href="index.html">bookingsiden</a>, og trykke "Book grupperom".
+  </section>
+      
+      <section class="spørsmål">
+   <span>Q: </span>Kan jeg bruke rommet dersom de som har booket det ikke er der?
+  </section>
+  <section class="svar">
+   <span>A: </span>Dersom rommet er booket men ikke blir brukt kan det brukes av hvem som helst til de som har booket det ankommer.
+  </section>
+      
+      <section class="spørsmål">
+   <span>Q: </span>Hva gjør man dersom noen nekter å forlate et rom de ikke har booket til de som har booket?
+  </section>
+  <section class="svar">
+   <span>A: </span>I henhold til skolereglementet er slik oppførsel ikke godkjent, og vil dermed medføre en advarsel og/eller straff til den skyldige parten.
+  </section>
+      
+      <section class="spørsmål">
+   <span>Q: </span>Hvordan kansellerer man en booking?
+  </section>
+  <section class="svar">
+   <span>A: </span>Man kan kanselere en booking ved  å gå inn på bookingen og «avbestille». 
+  </section>
+      
+      <section class="spørsmål">
+   <span>Q: </span>Hvordan re-booker jeg?
+  </section>
+  <section class="svar">
+   <span>A: </span>Dersom du booker et nytt lokale vil du miste din eksisterende booking.
+  </section>
 
-	<!-- Latest compiled and minified JavaScript -->
-<script src="js/bootstrap-datepicker.js"></script>
-<script src="js/npm.js"></script>
-<script src="js/html5shiv.min.js"></script>
-<script src="js/bootstrap.js"></script>
-<script src="js/jquery-2.1.3.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
+          <!-- Div som fungerer som en knapp til hoversiden for booking, da hele diven er wrappet i en link til index.html. -->
+  <a href="login.html"><div id="booking" class="bokser">
+      <h1>Book grupperom nå!</h1>
+      <p>Trykk her for å booke grupperom.</p></div></a>
+  </div>
+</div>
+    
+
+</section>
+
 </body>
+
+
+    
+
 </html>
